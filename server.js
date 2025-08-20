95b5d5a7c7dcb8073a00389fc6eab8b1740347de
hangtaip <hangtaip.stabilize940@passinbox.com>
2025-08-20 17:16:17 +0800

    index.html in dist

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const __devPath = `${__dirname}/dist`;
 
-app.use(express.static(__dirname));
+app.use(express.static(__devPath));
 
 app.use((req, res) => {
    res.status(404).sendFile(path.join(__dirname, "404.html"));
